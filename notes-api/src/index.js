6fe84c428b3abc7f728a403da45ecadaa3c3e36e
Jayanta Adhikary <jayantaadhikary@protonmail.com>
2023-10-21 10:48:19 +0530

    Create Notes API for CRUD operations

---
+++ notes-api/src/index.js
@@ -0,0 +1,70 @@
+const express = require("express");
+
+require("./db/mongoose");
+
+const Note = require("./models/note");
+
+const app = express();
+
+app.use(express.json());
+
+//CRUD
+// Create
+app.post("/notes", async (req, res) => {
+  const note = new Note(req.body);
+
+  try {
+    await note.save();
+    res.status(201).send(note);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
+
+// Read
+app.get("/notes", async (req, res) => {
+  try {
+    const notes = await Note.find({});
+    res.send(notes);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+// Update
+app.patch("/notes/:id", async (req, res) => {
+  try {
+    const note = await Note.findById(req.params.id);
+
+    if (!note) {
+      return res.status(404).send();
+    }
+
+    note.note = req.body.note;
+
+    await note.save();
+
+    res.status(200).send(note);
+  } catch (err) {
+    res.status(404).send(err);
+  }
+});
+
+// Delete
+app.delete("/notes/:id", async (req, res) => {
+  try {
+    const note = await Note.findByIdAndDelete(req.params.id);
+
+    if (!note) {
+      return res.status(404).send();
+    }
+
+    res.send("The note has been deleted");
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+app.listen(5000, () => {
+  console.log("Server is running on port 5000");
+});
